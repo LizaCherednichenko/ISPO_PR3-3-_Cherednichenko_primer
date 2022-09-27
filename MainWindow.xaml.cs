bc4e3bb16e81a478deea26ba9dceae9baa9a3446
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-09-27 12:32:37 +1000

    изменения 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,26 @@ namespace ISPO_PR3_3__Cherednichenko_primer
         {
             InitializeComponent();
         }
+
+        private void Pusk_Click(object sender, RoutedEventArgs e)
+        {
+            //Получение исходных данных из TextBox
+            double x = Convert.ToDouble(X.Text);
+            double y = Convert.ToDouble(Y.Text);
+            double z = Convert.ToDouble(Z.Text);
+
+            //Ввод исходных данных в окно результатов
+            Itog.Text = "Результаты работы программы ст. гр. 402 ИСП Чередниченко Е.К." + Environment.NewLine;
+            Itog.Text += "При X = " + X.Text + Environment.NewLine;
+            Itog.Text += "При Y = " + Y.Text + Environment.NewLine;
+            Itog.Text += "При Z = " + Z.Text + Environment.NewLine;
+
+            //Определение номера выбранной функции
+            int n = 0;
+            double u;
+
+
+
+        }
     }
 }
